39f5e1166b9eac9168471dc6d1dd6276c0c4f20b
aarontravass <43901677+aarontravass@users.noreply.github.com>
2020-04-06 13:34:37 +0530

    Rectified some bugs

--- Hill Cipher/hillcipher.cpp
+++ Hill Cipher/hillcipher.cpp
@@ -1,27 +1,29 @@
 #include <bits/stdc++.h>
+#define forn(i,n) for(int i=0;i<(n);i++)
+
 using namespace std;
 
 
-int gcdExtended(int a, int b, int &x, int &y) 
-{ 
-    if (a == 0) 
-    { 
-        x = 0, y = 1; 
-        return b; 
+int gcdExtended(int a, int b, int &x, int &y)
+{
+    if (a == 0)
+    {
+        x = 0, y = 1;
+        return b;
     }
-    int x1, y1, gcd = gcdExtended(b%a, a, x1, y1); 
-    x = y1 - (b/a) * x1; 
-    y = x1; 
-    return gcd; 
-} 
+    int x1, y1, gcd = gcdExtended(b%a, a, x1, y1);
+    x = y1 - (b/a) * x1;
+    y = x1;
+    return gcd;
+}
 
-int modInverse(int a, int m) 
-{ 
-    int x, y, g = gcdExtended(a, m, x, y); 
-    if (g != 1) return -1; 
-    else return (x%m + m) % m; 
-} 
-  
+int modInverse(int a, int m)
+{
+    int x, y, g = gcdExtended(a, m, x, y);
+    if (g != 1) return -1;
+    else return (x%m + m) % m;
+}
+
 
 
 int matrixmultiply(int a,int b,int c,int d){
@@ -43,7 +45,7 @@ vector<vector<int>> crypt(vector<vector<int>> &key,vector<vector<int>> &text){
 vector<vector<int>> getinverse(vector<vector<int>> &key){
     int det=key[0][0]*key[1][1]-key[0][1]*key[1][0];
     int mod=modInverse(det,26);
-    
+
     vector<vector<int>> inverted={
                 {(key[1][1])%26,(26-key[0][1])%26},
                 {(26-key[1][0])%26,(key[0][0])%26}
@@ -53,11 +55,11 @@ vector<vector<int>> getinverse(vector<vector<int>> &key){
 }
 
 int main() {
-	
+
 	string s,cip="",dcip="";
 	cout<<"Enter string (Uppercase only): "<<endl;
 	cin>>s;
-	vector<vector<int>> text(2,vector<int>(ceil(s.length()/2)));
+	vector<vector<int>> text(2,vector<int>(ceil((double)s.length()/2.0)));
 	int c=0;
 	for(int i=0;i<text[0].size();i++){
 	    for(int j=0;j<2;j++){
@@ -65,27 +67,36 @@ int main() {
 	        c++;
 	    }
 	}
+	if(s.length()%2==1){
+        text[1][text[1].size()-1]=0;
+	}
+	//cout<<text[0].size()<<endl;
 	vector<vector<int>> key={
         {3,3},
         {2,5}
 	};
 	vector<vector<int>> cipher=crypt(key,text);
-	
+
 	for(int i=0;i<cipher[0].size();i++){
 	    for(int j=0;j<2;j++){
-	        cip+=char(cipher[j][i]+65);
+	        cip+=char(cipher[j][i]+int('A'));
 	    }
 	}
+	if(s.length()%2==1) cip[cip.length()-1]='\0';
+
+
 	cout<<"Encrypted string is : "<<cip<<endl;
 	vector<vector<int>> inv=getinverse(key);
 	vector<vector<int>> decrypttext=crypt(inv,cipher);
+
+	for(int i=0;i<decrypttext[0].size();i++) for(int j=0;j<2;j++) dcip+=char(decrypttext[j][i]+int('A'));
+
+    	if(s.length()%2==1) dcip[dcip.length()-1]='\0';
 	
-	for(int i=0;i<inv[0].size();i++) for(int j=0;j<2;j++) dcip+=char(decrypttext[j][i]+65);
-	
-	cout<<"Decryptd string is : "<<dcip<<endl;
-	
-	
-	
-	
+	cout<<"Decrypted string is : "<<dcip<<endl;
+
+
+
+
 	return 0;
 }
